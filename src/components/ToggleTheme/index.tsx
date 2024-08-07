fa2fa72a7e2f5ad68ab6165bdb904142bd98dba3
du.nguyen <du.nguyen@asnet.com.vn>
2024-08-07 16:35:53 +0700

    feat: split file component

---
+++ src/components/ToggleTheme/index.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import Moon from '@/icons/Moon';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+
+// Components
+import { Button } from '@nextui-org/button';
+
+interface ToggleThemeProps {
+  className?: string;
+}
+
+const ToggleTheme = ({ className }: ToggleThemeProps) => {
+  const { theme, setTheme } = useTheme();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  const isDark = theme === 'dark';
+
+  return (
+    <Button
+      variant="light"
+      onClick={() => {
+        setTheme(isDark ? 'light' : 'dark');
+      }}
+      className={className}
+    >
+      <Moon isDark={isDark} />
+    </Button>
+  );
+};
+
+export default ToggleTheme;
--- src/layouts/Navbar/index.tsx
+++ src/layouts/Navbar/index.tsx
@@ -1,10 +1,4 @@
-'use client';
-
-import Moon from '@/icons/Moon';
-import { useTheme } from 'next-themes';
-import { Button } from '@nextui-org/button';
-
-import { useEffect, useState } from 'react';
+import { ToggleTheme } from '@/components';
 
 const Navbar = () => {
   return (
@@ -15,32 +9,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-interface ToggleThemeProps {
-  className?: string;
-}
-
-const ToggleTheme = ({ className }: ToggleThemeProps) => {
-  const { theme, setTheme } = useTheme();
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) return null;
-
-  const isDark = theme === 'dark';
-
-  return (
-    <Button
-      variant="light"
-      onClick={() => {
-        setTheme(isDark ? 'light' : 'dark');
-      }}
-      className={className}
-    >
-      <Moon isDark={isDark} />
-    </Button>
-  );
-};
