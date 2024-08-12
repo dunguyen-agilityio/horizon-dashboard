84c252338f5af874035a49b5f2a4fa583aecc76b
du.nguyen <du.nguyen@asnet.com.vn>
2024-08-12 14:23:12 +0700

    feat: define task component

---
+++ src/ui/kanban/Task/Task.test.tsx
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+
+import { MOCK_TASKS } from '@/mocks/task';
+
+import { Task } from '.';
+
+const mockTask = MOCK_TASKS[0];
+
+describe('Task tests', () => {
+  it('Should match snapshot', () => {
+    const { container } = render(<Task {...mockTask} />);
+    expect(container).toMatchSnapshot();
+  });
+});
