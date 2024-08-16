33f9965d011e70c55ae46f2a8484b34ccbb90bab
Viet Pham <viet.pham@asnet.com.vn>
2024-08-16 17:36:55 +0700

    (Feat: #8) Define Cart, Chart, Kanban, Lock, User icon

---
+++ src/icons/index.ts
@@ -0,0 +1,10 @@
+export { default as Add } from './Add';
+export { default as Cart } from './Cart';
+export { default as Chart } from './Chart';
+export { default as Moon } from './Moon';
+export { default as Notify } from './Notify';
+export { default as Kanban } from './Kanban';
+export { default as User } from './User';
+export { default as Pen } from './Pen';
+export { default as Lock } from './Lock';
+export { default as Search } from './Search';
