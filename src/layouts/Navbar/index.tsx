2336d9d5683af0236ebb3357469d9e0127fdd818
Viet Pham <viet.pham@asnet.com.vn>
2024-08-16 09:47:14 +0700

    (Feat #33): Implement Brand inside Navbar

--- src/layouts/Navbar/index.tsx
+++ src/layouts/Navbar/index.tsx
@@ -1,8 +1,9 @@
-import { ToggleTheme } from '@/components';
+import { ToggleTheme, Brand } from '@/components';
 
 const Navbar = () => {
   return (
-    <div className="bg-white dark:bg-secondary">
+    <div className="bg-white dark:bg-indigo w-[290px]">
+      <Brand />
       <ToggleTheme className="h-[60px] min-w-[60px] rounded-full flex justify-center items-center bg-gradient-primary" />
     </div>
   );
