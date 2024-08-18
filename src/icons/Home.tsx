ae7020bcda724ac110559f5bd6a1bb777c55d0b4
Viet Pham <viet.pham@asnet.com.vn>
2024-08-18 21:07:16 +0700

    (Feat: #8) Refactor style of Cart, Chart, Home, Kanban, Lock and User icon

---
+++ src/icons/Home.tsx
@@ -0,0 +1,6 @@
+const Home = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={20} height={17}>
+    <path d="M8 16v-5h4v5c0 .55.45 1 1 1h3c.55 0 1-.45 1-1V9h1.7c.46 0 .68-.57.33-.87L10.67.6c-.38-.34-.96-.34-1.34 0L.97 8.13c-.34.3-.13.87.33.87H3v7c0 .55.45 1 1 1h3c.55 0 1-.45 1-1Z" />
+  </svg>
+);
+export default Home;
