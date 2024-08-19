d78a8006801c086a413845976e5b578491d8aeee
Viet Pham <viet.pham@asnet.com.vn>
2024-08-19 11:12:29 +0700

    (Feat: #39) Update UI file not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,25 @@
 import Link from 'next/link';
+import { Text } from '@/components';
+
+// Types
+import { TEXT_SIZE } from '@/types/text';
 
 const NotFound = () => (
   <div className="w-full h-screen flex flex-col justify-center items-center bg-gray-100">
     <div className="text-center">
-      <h2 className="text-4xl font-bold text-gray-800 mb-4">Page Not Found</h2>
+      <Text
+        as="h1"
+        size={TEXT_SIZE['2xl']}
+        className="mb-4 dark:text-white text-indigo-dark"
+      >
+        Page Not Found
+      </Text>
       <p className="text-lg text-gray-600 mb-8">
         Sorry, the page you are looking for doesn&apos;t exist or has been
         moved.
       </p>
       <Link href="/">
-        <p className="inline-block bg-blue-500 text-white py-3 px-6 rounded-full text-lg font-semibold shadow-lg hover:bg-blue-600 transition duration-300">
+        <p className="inline-block bg-blue-450 text-white py-3 px-6 rounded-full text-lg font-semibold shadow-lg hover:bg-blue-600">
           Return Home
         </p>
       </Link>
