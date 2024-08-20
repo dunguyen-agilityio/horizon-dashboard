20214f1b945e008982716a5abd94b7ace4a65a00
Viet Pham <viet.pham@asnet.com.vn>
2024-08-20 09:11:37 +0700

    (Feat: #38) Implement UI file error.tsx

---
+++ src/app/(workspace)/error.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+// Components
+import { Button, Text } from '@/components';
+
+// Types
+import { TEXT_SIZE } from '@/types/text';
+
+const Error = ({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) => (
+  <div className="w-full h-screen flex flex-col justify-center items-center bg-gray-100">
+    <div className="text-center">
+      <Text
+        as="h1"
+        size={TEXT_SIZE['2xl']}
+        className="mb-4 dark:text-white text-indigo-dark"
+      >
+        Oops. Something went wrong!
+      </Text>
+      <Text className="text-lg dark:text-white text-indigo-dark mb-8 mx-4 md:mx-10 lg:mx-60 xl:mx-96 2xl:mx-[600px]">
+        {error.message
+          ? error.message
+          : 'Please try refreshing or return to the previous page.'}
+      </Text>
+      <Button color="danger" onClick={() => reset()}>
+        Try again
+      </Button>
+    </div>
+  </div>
+);
+
+export default Error;
