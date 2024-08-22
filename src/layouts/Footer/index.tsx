9a202a271a3e305e31b1e92d34895098372ec8e6
Viet Pham <viet.pham@asnet.com.vn>
2024-08-22 15:09:07 +0700

    (Feat: #10) Implement Footer layout component

---
+++ src/layouts/Footer/index.tsx
@@ -0,0 +1,29 @@
+// Components
+import { Text } from '@/components';
+import { Link } from '@nextui-org/link';
+
+// Types
+import { TEXT_SIZE } from '@/types/text';
+
+// Constants
+import { FOOTER_ROUTES } from '@/constants/routes';
+import { FOOTER_CONTENT } from '@/constants/text';
+
+const Footer = () => (
+  <div className="flex flex-col justify-between items-center px-1 gap-2 lg:gap-0 lg:flex-row xl:px-7">
+    <Text className="text-secondary text-center" size={TEXT_SIZE.sm}>
+      {FOOTER_CONTENT}
+    </Text>
+    <div className="flex gap-7 lg:gap-11">
+      {FOOTER_ROUTES.map((footer) => (
+        <Link href={footer.href} key={`${footer.title}`}>
+          <Text size={TEXT_SIZE.sm} className="text-secondary leading-6">
+            {footer.title}
+          </Text>
+        </Link>
+      ))}
+    </div>
+  </div>
+);
+
+export default Footer;
