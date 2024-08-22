214b7cb41d25608f64014e43c4d9cb95bd21f993
Viet Pham <viet.pham@asnet.com.vn>
2024-08-22 09:03:43 +0700

    (Feat: #46) Define type UserSignin and update prop InputPassword component

--- src/types/authentication.ts
+++ src/types/authentication.ts
@@ -1,6 +1,4 @@
-import { z } from 'zod';
-
-// Schema
-import { UserSigninFormDataSchema } from '@/constants/schema';
-
-export type UserSignin = z.infer<typeof UserSigninFormDataSchema>;
+export type UserSignin = {
+  email: string;
+  password: string;
+};
