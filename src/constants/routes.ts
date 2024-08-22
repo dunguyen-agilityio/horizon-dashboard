629b0c5f403b197c2d6eec0f0cc9697cfc257c68
du.nguyen <du.nguyen@asnet.com.vn>
2024-08-22 16:10:03 +0700

    feat: define constant for route endpoint

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,13 +1,22 @@
 import { Route } from '@/types/route';
 
+export const ENDPOINTS = {
+  DASHBOARD: '/dashboard',
+  NFT_MARKETPLACE: '/nft-marketplace',
+  CONTRIBUTOR: '/contributor',
+  KANBAN: '/kanban',
+  PROFILE: '/profile',
+  SIGN_IN: '/sign-in',
+};
+
 export const ROUTES: Route[] = [
   {
     title: 'Main Dashboard',
-    href: '/dashboard',
+    href: ENDPOINTS.DASHBOARD,
   },
-  { title: 'NFT Marketplace', href: '/nft-marketplace' },
-  { title: 'Contributor', href: '/contributor' },
-  { title: 'Kanban', href: '/kanban' },
-  { title: 'Profile', href: '/profile' },
-  { title: 'Sign In', href: '/sign-in' },
+  { title: 'NFT Marketplace', href: ENDPOINTS.NFT_MARKETPLACE },
+  { title: 'Contributor', href: ENDPOINTS.CONTRIBUTOR },
+  { title: 'Kanban', href: ENDPOINTS.KANBAN },
+  { title: 'Profile', href: ENDPOINTS.PROFILE },
+  { title: 'Sign In', href: ENDPOINTS.SIGN_IN },
 ];
