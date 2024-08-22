9319fa4305236f0d5ee9a460e090378a039d799c
du.nguyen <du.nguyen@asnet.com.vn>
2024-08-22 10:49:15 +0700

    fix: fix conflict

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -6,8 +6,3 @@ export const STARBUCKS_IMAGE = {
 export const NFT_IMAGES = {
   COVER: '/nft-cover.webp',
 };
-
-export const STARBUCKS_IMAGE = {
-  COVER: '/starbucks-cover.webp',
-  LOGO: '/starbucks-logo.webp',
-};
