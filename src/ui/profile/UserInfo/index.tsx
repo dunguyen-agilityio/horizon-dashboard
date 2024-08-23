bd773d60d7ed377b0a12b0766e6951fefbd5c0b5
Viet Pham <viet.pham@asnet.com.vn>
2024-08-23 09:26:30 +0700

    (Feat: #62) Implement UserInfo component

--- src/ui/profile/UserInfo/index.tsx
+++ src/ui/profile/UserInfo/index.tsx
@@ -3,26 +3,32 @@ import Image from 'next/image';
 import { Avatar } from '@nextui-org/avatar';
 import { Text } from '@/components';
 
+// Constants
+import { PROFILE_IMAGES } from '@/constants/images';
+
 // Mocks
 import { MOCK_USERS } from '@/mocks/user';
 
 // Types
 import { TEXT_SIZE, TEXT_VARIANT } from '@/types/text';
-import { PROFILE_IMAGES } from '@/constants/images';
 
 const { BACKGROUND } = PROFILE_IMAGES;
-const { avatar, fullName, userName, role } = MOCK_USERS[0];
+const { avatar, fullName, userName, role, posts, followers, following } =
+  MOCK_USERS[0];
 
 const UserInfo = () => (
-  <div className="h-[365px] dark:bg-indigo bg-white w-[552px] p-[18px] rounded-md">
-    <div className="w-[518px] h-[131px] relative">
+  <div className="dark:bg-indigo bg-white w-[350px] lg:w-[430px] xl:w-[552px] p-[17px] rounded-md">
+    <div className="w-[315px] lg:w-[396px] xl:w-[518px] h-[131px] relative">
       <Image
+        fill
         src={BACKGROUND}
         alt="background-profile"
-        fill
-        className="object-cover"
+        className="object-cover rounded-2xl"
+        sizes="(max-width: 768px) 315px,
+              (min-width: 769px) and (max-width: 1024px) 396px,
+              (min-width: 1025px) 518px"
       />
-      <div className="absolute top-[85px] left-[215px]">
+      <div className="absolute top-[85px] left-[117px] lg:left-[154px] xl:left-[215px]">
         <Avatar
           src={avatar}
           ImgComponent={Image}
@@ -45,39 +51,39 @@ const UserInfo = () => (
           {role}
         </Text>
       </div>
-      <div className="flex justify-center gap-11 mt-[26px]">
-        <div className="">
-          <Text size={TEXT_SIZE.extra} className="font-extrabold">
-            77
+      <div className="flex justify-center gap-11 mt-[26px] mb-5">
+        <div className="w-14 text-center">
+          <Text size={TEXT_SIZE.extra} className="font-extrabold leading-6">
+            {posts}
           </Text>
           <Text
             size={TEXT_SIZE.sm}
             variant={TEXT_VARIANT.SECONDARY}
-            className="text-center"
+            className="leading-5"
           >
             Posts
           </Text>
         </div>
-        <div className="">
-          <Text size={TEXT_SIZE.extra} className="font-extrabold">
-            7.9k
+        <div className="w-14 text-center">
+          <Text size={TEXT_SIZE.extra} className="font-extrabold leading-6">
+            {`${followers}k`}
           </Text>
           <Text
             size={TEXT_SIZE.sm}
             variant={TEXT_VARIANT.SECONDARY}
-            className="text-center"
+            className="leading-5"
           >
             Followers
           </Text>
         </div>
-        <div className="">
-          <Text size={TEXT_SIZE.extra} className="font-extrabold">
-            365
+        <div className="w-14 text-center">
+          <Text size={TEXT_SIZE.extra} className="font-extrabold leading-6">
+            {following}
           </Text>
           <Text
             size={TEXT_SIZE.sm}
             variant={TEXT_VARIANT.SECONDARY}
-            className="text-center"
+            className="leading-5"
           >
             Following
           </Text>
