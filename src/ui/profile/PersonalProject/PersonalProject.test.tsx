16170244356bc2f8b7263203b7de5a51958520cf
Viet Pham <viet.pham@asnet.com.vn>
2024-08-27 15:31:47 +0700

    (Feat: #65) Add more test case for PersonalProjects

--- src/ui/profile/PersonalProject/PersonalProject.test.tsx
+++ src/ui/profile/PersonalProject/PersonalProject.test.tsx
@@ -7,4 +7,15 @@ describe('PersonalProjects tests', () => {
     const { container } = render(<PersonalProjects userId={3} />);
     expect(container).toMatchSnapshot();
   });
+
+  it('Should render correct title and description', () => {
+    const { getByText } = render(<PersonalProjects userId={3} />);
+
+    const title = getByText('All Projects 3');
+    const description = getByText(
+      'Here you can find more details about your projects. Keep you user engaged by providing meaningful information.',
+    );
+    expect(title).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+  });
 });
