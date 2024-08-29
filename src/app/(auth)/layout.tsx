1492ab142c0f150c835a98bf5c87b26b4a141358
du.nguyen <du.nguyen@asnet.com.vn>
2024-08-29 11:02:54 +0700

    fix: update auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -10,39 +10,44 @@ import { FOOTER_ROUTES } from '@/constants/routes';
 import { TEXT_SIZE } from '@/types/text';
 
 const AuthenticateLayout = ({ children }: React.PropsWithChildren) => (
-  <main className="flex bg-white dark:bg-indigo-dark px-6 sm:p-0 relative">
-    {children}
-
-    {/* Wallpaper */}
-    <div className="hidden 2xl:block">
-      <div className="w-[965px] h-[1152px] relative">
-        <Image
-          src="/wallpaper.webp"
-          alt="wall-paper"
-          fill
-          style={{
-            objectFit: 'contain',
-          }}
-        />
-        <div className="bottom-7 right-7 absolute">
-          <ToggleTheme variant="secondary" />
-        </div>
-        <div className="absolute bottom-12 left-72">
-          <footer className="flex justify-between">
-            <div className="flex gap-11">
-              {FOOTER_ROUTES.map((footer) => (
-                <Link href={footer.href} key={`${footer.title}`}>
-                  <Text as="span" className="text-white" size={TEXT_SIZE.sm}>
-                    {footer.title}
-                  </Text>
-                </Link>
-              ))}
-            </div>
-          </footer>
+  <div className="flex bg-white dark:bg-indigo-dark px-6 sm:p-0 h-screen">
+    <main className="flex-1 flex justify-center py-10">
+      <div className="w-fit flex flex-col justify-between">
+        {children}
+        <div className="h-[60px] flex items-center">
+          <Text className="text-secondary text-center" size={TEXT_SIZE.sm}>
+            © 2022 Horizon UI. All Rights Reserved. Made with love by Simple!
+          </Text>
         </div>
       </div>
+    </main>
+
+    <div className="w-1/2 2xl:w-2/5 h-screen relative hidden xl:flex justify-center items-end">
+      <Image
+        src="/wallpaper.webp"
+        alt="wall-paper"
+        fill
+        sizes="(min-width: 1040px) 100vw, 0px"
+        style={{
+          objectFit: 'fill',
+        }}
+      />
+
+      <footer className="pb-12 flex gap-11">
+        {FOOTER_ROUTES.map((footer) => (
+          <Link href={footer.href} key={`${footer.title}`}>
+            <Text as="span" className="text-white" size={TEXT_SIZE.sm}>
+              {footer.title}
+            </Text>
+          </Link>
+        ))}
+      </footer>
     </div>
-  </main>
+    <ToggleTheme
+      variant="secondary"
+      className="absolute bottom-0 xl:bottom-7 right-7"
+    />
+  </div>
 );
 
 export default AuthenticateLayout;
--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,7 @@
 // Components
 import { Link } from '@nextui-org/link';
 import { Divider } from '@nextui-org/divider';
-import { BoxIcon, ToggleTheme, Text } from '@/components';
+import { BoxIcon, Text } from '@/components';
 import SignInContent from '@/ui/auth/SignInContent';
 
 // Icons
@@ -11,44 +11,33 @@ import { ArrowRight } from '@/icons';
 import { TEXT_SIZE, TEXT_VARIANT } from '@/types/text';
 
 const SignInPage = () => (
-  <div className="flex flex-1 justify-center">
-    <div className="h-full py-10 flex flex-col">
-      {/* Navigate Back */}
-      <div className="flex justify-between">
-        <Link href="/dashboard">
-          <BoxIcon icon={<ArrowRight />} customClass="fill-secondary" />
+  <>
+    {/* Navigate Back */}
+    <Link href="/dashboard">
+      <BoxIcon icon={<ArrowRight />} customClass="fill-secondary" />
 
-          <Text variant={TEXT_VARIANT.SECONDARY} className="ml-1">
-            Back to dashboard
-          </Text>
-        </Link>
-        <div className="2xl:hidden">
-          <ToggleTheme variant="primary" />
-        </div>
-      </div>
-
-      {/* Sign In Content */}
-      <div className="mt-14 mb-36 sm:mt-[200px] sm:mb-[290px] 2xl:mt-[250px]">
-        <Text as="h1" size={TEXT_SIZE['2xl']} className="leading-[56px]">
-          Sign In
-        </Text>
-        <Text
-          as="h2"
-          size={TEXT_SIZE['md']}
-          variant={TEXT_VARIANT.TERTIARY}
-          className="leading-6"
-        >
-          Enter your username or email with password to sign in!
-        </Text>
-        <Divider className="bg-amber-450 dark:bg-slate-700 mt-6 mb-3" />
-        <SignInContent />
-      </div>
-
-      <Text className="text-secondary text-center" size={TEXT_SIZE.sm}>
-        © 2022 Horizon UI. All Rights Reserved. Made with love by Simple!
+      <Text variant={TEXT_VARIANT.SECONDARY} className="ml-1">
+        Back to dashboard
       </Text>
+    </Link>
+
+    {/* Sign In Content */}
+    <div className="pt-14 pb-36 sm:pt-[200px] sm:pb-[290px] 2xl:pt-[250px]">
+      <Text as="h1" size={TEXT_SIZE['2xl']} className="leading-[56px]">
+        Sign In
+      </Text>
+      <Text
+        as="h2"
+        size={TEXT_SIZE['md']}
+        variant={TEXT_VARIANT.TERTIARY}
+        className="leading-6"
+      >
+        Enter your username or email with password to sign in!
+      </Text>
+      <Divider className="bg-amber-450 dark:bg-slate-700 mt-6 mb-3" />
+      <SignInContent />
     </div>
-  </div>
+  </>
 );
 
 export default SignInPage;
