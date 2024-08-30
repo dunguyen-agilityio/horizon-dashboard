87256c647a03171634491d12cc9ce3ce6a00c806
Viet Pham <viet.pham@asnet.com.vn>
2024-08-30 14:27:02 +0700

    (Feat: #74) Config Firebase and store env key

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,15 +1,26 @@
-import { initializeApp } from 'firebase/app';
+'use client';
 
+import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_APP_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_MEASUREMENT,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_SENDER_ID,
+  FIREBASE_STORAGE_BUCKET,
+} from '@/constants/environment';
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyAwE0j6VyQ6Xb9kl_cr-oSwYM2DKLT20Jo',
-  authDomain: 'horizon-dashboard-4bffe.firebaseapp.com',
-  projectId: 'horizon-dashboard-4bffe',
-  storageBucket: 'horizon-dashboard-4bffe.appspot.com',
-  messagingSenderId: '742731045164',
-  appId: '1:742731045164:web:c85eccd119d92416907489',
-  measurementId: 'G-TL9295T2FX',
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT,
 };
 
 export const app = initializeApp(firebaseConfig);
