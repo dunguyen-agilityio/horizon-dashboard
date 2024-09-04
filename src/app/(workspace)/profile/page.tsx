53fefed982652ade6ca3d2569f825fe0e5349082
Viet Pham <viet.pham@asnet.com.vn>
2024-09-04 16:09:57 +0700

    (Feat: #84) Implement Profile page

--- src/app/(workspace)/profile/page.tsx
+++ src/app/(workspace)/profile/page.tsx
@@ -1,5 +1,50 @@
+import {
+  GeneralInformation,
+  PersonalProject,
+  Storage,
+  Upload,
+  UserInfo,
+  ControlBoard,
+} from '@/ui/profile';
+import { MOCK_USERS } from '@/mocks/user';
+
 const ProfilePage = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="mx-auto max-w-[1610px]">
+      <div className="grid grid-cols-12 grid-rows-2 2xl:grid-rows-1 gap-5 mb-5">
+        <div className="col-span-12 2xl:col-span-4">
+          <UserInfo
+            role="Design NFT"
+            userName={MOCK_USERS[0].userName}
+            avatar={MOCK_USERS[1].avatar}
+            followersCount={33}
+            followingCount={90}
+            postsTotal={20}
+          />
+        </div>
+        <div className="col-span-12 lg:col-span-4 xl:col-span-6 2xl:col-span-3">
+          <Storage totalSize={20} usedSize={10} valueProgress={50} />
+        </div>
+        <div className="col-span-12 lg:col-span-8 xl:col-span-6 2xl:col-span-5">
+          <Upload />
+        </div>
+      </div>
+      {/*  */}
+      <div className="grid grid-cols-12 grid-rows-1 gap-5">
+        <div className="col-span-12 lg:col-span-12 xl:col-span-12 2xl:col-span-4">
+          <div className="flex h-full justify-center items-center">
+            <PersonalProject userId={1} />
+          </div>
+        </div>
+        <div className="col-span-12 lg:col-span-8 xl:col-span-8 2xl:col-span-5">
+          <GeneralInformation />
+        </div>
+        <div className="col-span-12 lg:col-span-4 2xl:col-span-3">
+          <ControlBoard />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
