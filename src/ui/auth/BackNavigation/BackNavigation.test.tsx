ce0a24e24b400998bd6fcd8ab565239857c938bc
du.nguyen <du.nguyen@asnet.com.vn>
2024-09-06 09:40:34 +0700

    feat: remove redundant code

--- src/ui/auth/BackNavigation/BackNavigation.test.tsx
+++ src/ui/auth/BackNavigation/BackNavigation.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import BackNavigation from '.';
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import {
   AUTH_NAVIGATION_HEADER,
   AUTH_ROUTES,
@@ -8,7 +8,6 @@ import {
 } from '@/constants';
 
 jest.mock('next/navigation', () => ({
-  useSearchParams: jest.fn(),
   usePathname: jest.fn(),
 }));
 
@@ -21,26 +20,6 @@ describe('BackNavigation tests', () => {
     (usePathname as jest.Mock).mockImplementation(
       () => AUTH_ROUTES.FORGET_PASSWORD,
     );
-    (useSearchParams as jest.Mock).mockImplementation(
-      () => new URLSearchParams({ step: '2' }),
-    );
-
-    const { queryByText } = render(<BackNavigation />);
-
-    expect(
-      queryByText(
-        `Back to ${AUTH_NAVIGATION_HEADER[AUTH_ROUTES.FORGET_PASSWORD]}`,
-      ),
-    ).toBeInTheDocument();
-  });
-
-  it('Should render correctly', () => {
-    (usePathname as jest.Mock).mockImplementation(
-      () => AUTH_ROUTES.FORGET_PASSWORD,
-    );
-    (useSearchParams as jest.Mock).mockImplementation(
-      () => new URLSearchParams({ step: '1' }),
-    );
 
     const { queryByText } = render(<BackNavigation />);
 
@@ -51,9 +30,6 @@ describe('BackNavigation tests', () => {
 
   it('Should render correctly', () => {
     (usePathname as jest.Mock).mockImplementation(() => AUTH_ROUTES.SIGN_UP);
-    (useSearchParams as jest.Mock).mockImplementation(
-      () => new URLSearchParams({ step: '1' }),
-    );
 
     const { queryByText } = render(<BackNavigation />);
 
@@ -65,9 +41,6 @@ describe('BackNavigation tests', () => {
   it('Should render correctly', () => {
     (usePathname as jest.Mock).mockImplementation(
       () => AUTH_ROUTES.RESET_PASSWORD,
-    );
-    (useSearchParams as jest.Mock).mockImplementation(
-      () => new URLSearchParams({ step: '1' }),
     );
 
     const { queryByText } = render(<BackNavigation />);
