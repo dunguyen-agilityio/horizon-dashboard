3629fcecfc7bd2b5567a72b074f4af0076ea22b3
du.nguyen <du.nguyen@asnet.com.vn>
2024-09-09 16:55:09 +0700

    feat: remove redundant code

--- __mocks__/next-auth/index.ts
+++ __mocks__/next-auth/index.ts
@@ -1,16 +1,13 @@
 import { NextRequest } from 'next/server';
 
-const NextAuth = () => {
-  console.log('first');
-  return {
-    handlers: {
-      GET: (_: NextRequest) => Promise<Response>,
-      POST: (_: NextRequest) => Promise<Response>,
-    },
-    signIn: async () => {},
-    signOut: async () => {},
-    auth: async () => {},
-  };
-};
+const NextAuth = () => ({
+  handlers: {
+    GET: (_: NextRequest) => Promise<Response>,
+    POST: (_: NextRequest) => Promise<Response>,
+  },
+  signIn: async () => {},
+  signOut: async () => {},
+  auth: async () => {},
+});
 
 export default NextAuth;
