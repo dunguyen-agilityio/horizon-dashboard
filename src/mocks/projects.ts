f4dc4461e4959671c4a09274378f2738b4cd1af2
Viet Pham <viet.pham@asnet.com.vn>
2024-09-09 08:26:15 +0700

    Resolve conflict on rootlayout

---
+++ src/mocks/projects.ts
@@ -0,0 +1,19 @@
+import { IProjectCardProps } from '@/ui/profile/PersonalProject/ProjectCard';
+
+export const PROJECTS_MOCK: IProjectCardProps[] = [
+  {
+    baseProject: 'project #3',
+    id: '2',
+    title: 'Technology behind the Blockchain',
+  },
+  {
+    baseProject: 'project #3',
+    id: '1',
+    title: 'Greatest way to a good Economy',
+  },
+  {
+    baseProject: 'project #1',
+    id: '3',
+    title: 'Most essential tips for Burnout',
+  },
+];
