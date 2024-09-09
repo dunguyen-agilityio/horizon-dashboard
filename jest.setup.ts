83652562115bf74e8080a3f6df6ce66a47a41c98
du.nguyen <du.nguyen@asnet.com.vn>
2024-09-09 13:51:47 +0700

    fix: update setup testing

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,25 @@
 import '@testing-library/jest-dom';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn().mockImplementation(() => ({
+    push: jest.fn(),
+    replace: jest.fn(),
+  })),
+  usePathname: jest.fn(),
+  useSearchParams: jest.fn(),
+  useParams: jest.fn(),
+}));
