efc9420fddec0f46d9539f325ebfc2411c168674
Viet Pham <viet.pham@asnet.com.vn>
2024-09-09 13:58:21 +0700

    (Feat: #95) Refactor Navbar component

--- src/layouts/Navbar/index.tsx
+++ src/layouts/Navbar/index.tsx
@@ -4,7 +4,14 @@
 import { usePathname } from 'next/navigation';
 
 // Components
-import { Banner, NavItem } from '@/components';
+import { Banner, Brand, NavItem } from '@/components';
+import {
+  Navbar as NavComponent,
+  NavbarContent,
+  NavbarBrand,
+  Divider,
+  NavbarItem,
+} from '@nextui-org/react';
 
 // Constants
 import { ROUTES, AUTH_ROUTES } from '@/constants/routes';
@@ -17,26 +24,37 @@ const Navbar = ({ isAuthenticated }: NavbarProps) => {
   const pathname = usePathname();
 
   return (
-    <nav className="bg-white flex flex-col justify-between dark:bg-indigo w-[240px] lg:w-[290px] h-[1152px]">
-      <div className="flex flex-col justify-between pt-[38px] gap-5">
+    <NavComponent
+      className="bg-white w-[290px] dark:bg-indigo h-screen"
+      classNames={{
+        wrapper: 'px-0 flex-col h-full',
+      }}
+    >
+      <NavbarBrand className="flex-grow-0">
+        <div className="flex flex-col w-[290px]">
+          <Brand />
+          <Divider className="bg-zinc-100 dark:bg-slate-700" />
+        </div>
+      </NavbarBrand>
+      <NavbarContent className="flex flex-1 w-full flex-col items-center justify-between pt-[38px] gap-5">
         {ROUTES.map(({ href, icon, title }, index) => {
           if (isAuthenticated && href === AUTH_ROUTES.SIGN_IN) return null;
-
           return (
-            <NavItem
-              key={`${title}-${index}`}
-              href={href}
-              icon={icon}
-              label={title}
-              isActive={href === pathname}
-            />
+            <NavbarItem key={`${title}-${index}`} className="w-full">
+              <NavItem
+                href={href}
+                icon={icon}
+                label={title}
+                isActive={href === pathname}
+              />
+            </NavbarItem>
           );
         })}
-      </div>
-      <div className="mb-6">
-        <Banner />
-      </div>
-    </nav>
+        <li className="flex-1 flex items-end mb-6">
+          <Banner />
+        </li>
+      </NavbarContent>
+    </NavComponent>
   );
 };
 
