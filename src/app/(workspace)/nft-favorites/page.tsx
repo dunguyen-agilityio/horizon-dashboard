f08e353fbe7c7d5d6a8ec11bec7aff2444a8be1b
Viet Pham <viet.pham@asnet.com.vn>
2024-09-10 14:29:20 +0700

    (Feat: #79) Define NFTFavorites componennt and implement NFTFavorites page

---
+++ src/app/(workspace)/nft-favorites/page.tsx
@@ -0,0 +1,8 @@
+// component
+import { NFTFavorites } from '@/ui/nft-marketplace';
+
+const NFTFavoritesPage = () => {
+  return <NFTFavorites />;
+};
+
+export default NFTFavoritesPage;
--- src/ui/nft-marketplace/NFTFavorites/index.tsx
+++ src/ui/nft-marketplace/NFTFavorites/index.tsx
@@ -1,36 +1,41 @@
+'use client';
+
+import { useState } from 'react';
+
 // Components
-import { Text } from '@/components';
 import NFTCard from '../NFTCard';
+import { Pagination } from '@nextui-org/pagination';
 
 // Mock
 import { MOCK_NFTS } from '@/mocks/nft';
 
-interface NFTFavoritesProps {
-  id: number;
-}
-
-const NFTFavorites = (_: NFTFavoritesProps) => {
-  // TODO: handle call api get NFT favorite list
+const NFTFavorites = () => {
+  // TODO: handle call api get NFT favorite list instead of using mock data
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
   return (
-    <>
-      <div className="flex justify-between pb-5 mb-5 lg:mb-0">
-        <Text className="font-bold text-[24px]">Favorite NFTs</Text>
-      </div>
-      <div className="flex justify-center">
-        <div className="flex flex-wrap gap-5 justify-center sm:justify-start max-w-[716px] xl:max-w-[1084px] 2xl:max-w-[1452px]">
-          {MOCK_NFTS.map((trend) => (
-            <NFTCard
-              isShowIcon={false}
-              key={trend.id}
-              {...trend}
-              data-testid={`nft-card-${trend.id}`}
-            />
-          ))}
+    <div className="flex justify-center mt-1 sm:mt-4">
+      <div className="flex flex-wrap justify-center sm:justify-start gap-5 max-w-[716px] xl:max-w-[1084px] 2xl:max-w-[1452px]">
+        {MOCK_NFTS.map((favorite) => (
+          <NFTCard
+            key={favorite.id}
+            data-testid={`nft-card-${favorite.id}`}
+            isShowIcon={false}
+            {...favorite}
+          />
+        ))}
+        <div className="w-full flex justify-center lg:justify-end my-8">
+          <Pagination
+            showControls
+            showShadow
+            color="primary"
+            total={7} // TODO: Will handle get page size
+            page={currentPage}
+            onChange={setCurrentPage}
+          />
         </div>
       </div>
-      {/* TODO: Will handle Pagination */}
-    </>
+    </div>
   );
 };
 
