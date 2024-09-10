bc7fbc39d3e9b25282895f0b0803a2078af6d44e
Viet Pham <viet.pham@asnet.com.vn>
2024-09-10 16:11:59 +0700

    (Feat: #104) Implement ProjectCard inside PersonalProject and update file snapshot

--- src/ui/profile/PersonalProject/PersonalProject.test.tsx
+++ src/ui/profile/PersonalProject/PersonalProject.test.tsx
@@ -4,14 +4,14 @@ import PersonalProjects from '.';
 
 describe('PersonalProjects tests', () => {
   it('Should match snapshot', () => {
-    const { container } = render(<PersonalProjects userId={3} />);
+    const { container } = render(<PersonalProjects />);
     expect(container).toMatchSnapshot();
   });
 
   it('Should render correct title and description', () => {
-    const { getByText } = render(<PersonalProjects userId={3} />);
+    const { getByText } = render(<PersonalProjects />);
 
-    const title = getByText('All Projects 3');
+    const title = getByText('All Projects');
     const description = getByText(
       'Here you can find more details about your projects. Keep you user engaged by providing meaningful information.',
     );
--- src/ui/profile/PersonalProject/index.tsx
+++ src/ui/profile/PersonalProject/index.tsx
@@ -6,11 +6,11 @@ import { TEXT_SIZE, TEXT_VARIANT } from '@/types/text';
 import ProjectCard from './ProjectCard';
 import { PROJECTS_MOCK } from '@/mocks/projects';
 
-const PersonalProjects = ({ userId }: { userId: number }) => (
+const PersonalProjects = () => (
   <div className="dark:bg-indigo bg-white py-6 px-8 rounded-md">
     <div className="text-center xl:text-start">
       <Text size={TEXT_SIZE.extra} className="font-extrabold leading-8 mb-3">
-        All Projects {userId}
+        All Projects
       </Text>
       <Text
         variant={TEXT_VARIANT.SECONDARY}
@@ -23,11 +23,10 @@ const PersonalProjects = ({ userId }: { userId: number }) => (
     <div className="flex flex-col gap-5 mb-2">
       {PROJECTS_MOCK.map(({ baseProject, id, title, imageProject }) => (
         <ProjectCard
+          key={id}
           baseProject={baseProject}
-          id={id}
           title={title}
           imageProject={imageProject}
-          key={id}
         />
       ))}
     </div>
