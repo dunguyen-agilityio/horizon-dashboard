2ee9170e8cea58b34646b8ebe417384bea3ef51c
du.nguyen <du.nguyen@asnet.com.vn>
2024-09-10 17:57:22 +0700

    fix: mock summary data

--- src/mocks/summaryCard.tsx
+++ src/mocks/summaryCard.tsx
@@ -3,8 +3,19 @@ import { SummaryCardProps, TrendInfo } from '@/ui/dashboard/SummaryCard';
 
 // icons
 import AddIcon from '@/icons/Add';
+import { Earning } from '@/types/earning';
 
 export type SummaryCard = SummaryCardProps & TrendInfo;
+
+export const MOCK_EARNINGS_COUNTER_DATA: Earning = {
+  earning: 350.5,
+  spend: 682.5,
+  sale: 574.34,
+  balance: 1000,
+  task: 154,
+  project: 2935,
+  trending: 23,
+};
 
 export const MOCK_SUMMARY_CARD: SummaryCard[] = [
   {
@@ -18,7 +29,6 @@ export const MOCK_SUMMARY_CARD: SummaryCard[] = [
     additionalContent: <AddIcon />,
   },
   {
-    variant: 'secondary',
     title: 'Earnings',
     counter: 350.5,
     trendValue: 20,
---
+++ src/types/earning.ts
@@ -0,0 +1,9 @@
+export interface Earning {
+  earning: number;
+  spend: number;
+  sale: number;
+  balance: number;
+  task: number;
+  project: number;
+  trending: number;
+}
