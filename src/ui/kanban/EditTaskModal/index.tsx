ee452b070a9db207c25619dc3a398ae4e5985d8d
Long Viet Tran <long.tran@asnet.com.vn>
2024-09-12 09:30:06 +0700

    Feat #117 Update UI for modal - Kanban

--- src/ui/kanban/EditTaskModal/index.tsx
+++ src/ui/kanban/EditTaskModal/index.tsx
@@ -70,6 +70,10 @@ export const EditTaskModal = ({
     [labels],
   );
 
+  const handleUpdateTask = () => {
+    // TODO: should handle when API already
+  };
+
   return (
     <Modal
       size="3xl"
@@ -77,94 +81,90 @@ export const EditTaskModal = ({
       onOpenChange={onOpenChange}
       placement="center"
       classNames={{
-        base: 'bg-white dark:bg-indigo',
+        base: 'bg-white dark:bg-indigo p-4 min-h-[480px] m-2',
       }}
     >
       <ModalContent>
-        {(onClose) => (
-          <>
-            <ModalHeader className="flex flex-col gap-1">
-              <Text size={TEXT_SIZE.lg} as="h1">
-                {title}
-              </Text>
-            </ModalHeader>
-            <ModalBody>
-              <div className="flex justify-between">
-                <div className="flex flex-col gap-4">
-                  <div className="flex items-center gap-2">
-                    <Text className="font-bold">Description</Text>
-                    <BoxIcon icon={<Note />} customClass="fill-secondary" />
-                  </div>
-                  <Textarea
-                    data-testid="textarea-description"
-                    rows={9}
-                    aria-label="textarea-description"
-                    defaultValue={description}
-                    disableAutosize
-                    classNames={{
-                      inputWrapper: 'w-[185px] sm:w-[450px] lg:w-[500px]',
-                      input:
-                        'group-data-[has-value=true]:text-primary group-data-[has-value=true]:dark:text-white',
-                    }}
-                  />
-                </div>
-                <div className="flex flex-col gap-2 items-center justify-center">
-                  <Select
-                    aria-label="select-label"
-                    label="Label"
-                    className="max-w-44"
-                    defaultSelectedKeys={defaultSelectedLabelKeys}
-                    classNames={selectStyle}
-                  >
-                    {labelValues.map((label) => (
-                      <SelectItem key={label.value}>{label.value}</SelectItem>
-                    ))}
-                  </Select>
-                  <Select
-                    aria-label="select-members"
-                    label="Assign members"
-                    selectionMode="multiple"
-                    defaultSelectedKeys={defaultSelectedMemberKeys}
-                    className="max-w-44"
-                    classNames={selectStyle}
-                  >
-                    {MOCK_USERS?.map(({ userName }) => (
-                      <SelectItem key={userName}>{userName}</SelectItem>
-                    ))}
-                  </Select>
-                  <Input
-                    aria-label="input-start-date"
-                    type="date"
-                    label="Start date"
-                    className="max-w-44"
-                    classNames={{
-                      label: 'group-data-[filled=true]:text-secondary',
-                      input:
-                        'group-data-[has-value=true]:text-primary group-data-[has-value=true]:dark:text-white',
-                      inputWrapper: '',
-                    }}
-                  />
-                  <Input
-                    aria-label="input-due-date"
-                    type="date"
-                    label="Due date"
-                    className="max-w-44"
-                    classNames={{
-                      label: 'group-data-[filled=true]:text-secondary',
-                      input:
-                        'group-data-[has-value=true]:text-primary group-data-[has-value=true]:dark:text-white',
-                    }}
-                  />
-                </div>
+        <ModalHeader className="flex flex-col gap-1">
+          <Text size={TEXT_SIZE.lg} as="h2">
+            {title}
+          </Text>
+        </ModalHeader>
+        <ModalBody>
+          <div className="flex flex-col justify-between lg:flex-row gap-4">
+            <div className="flex flex-col gap-4">
+              <div className="flex items-center gap-2">
+                <Text className="font-bold">Description</Text>
+                <BoxIcon icon={<Note />} customClass="fill-secondary" />
               </div>
-            </ModalBody>
-            <ModalFooter>
-              <Button color="primary" onPress={onClose}>
-                Update
-              </Button>
-            </ModalFooter>
-          </>
-        )}
+              <Textarea
+                data-testid="textarea-description"
+                rows={9}
+                aria-label="textarea-description"
+                defaultValue={description}
+                disableAutosize
+                classNames={{
+                  inputWrapper: 'w-full lg:w-[500px]',
+                  input:
+                    'group-data-[has-value=true]:text-primary group-data-[has-value=true]:dark:text-white',
+                }}
+              />
+            </div>
+            <div className="flex flex-row flex-wrap md:flex-nowrap lg:flex-col gap-2 items-center justify-center">
+              <Select
+                aria-label="select-label"
+                label="Label"
+                className="max-w-40"
+                defaultSelectedKeys={defaultSelectedLabelKeys}
+                classNames={selectStyle}
+              >
+                {labelValues.map((label) => (
+                  <SelectItem key={label.value}>{label.value}</SelectItem>
+                ))}
+              </Select>
+              <Select
+                aria-label="select-members"
+                label="Assign members"
+                selectionMode="multiple"
+                defaultSelectedKeys={defaultSelectedMemberKeys}
+                className="max-w-40"
+                classNames={selectStyle}
+              >
+                {MOCK_USERS?.map(({ userName }) => (
+                  <SelectItem key={userName}>{userName}</SelectItem>
+                ))}
+              </Select>
+              <Input
+                aria-label="input-start-date"
+                type="date"
+                label="Start date"
+                className="max-w-40"
+                classNames={{
+                  label: 'group-data-[filled=true]:text-secondary',
+                  input:
+                    'group-data-[has-value=true]:text-primary group-data-[has-value=true]:dark:text-white',
+                  inputWrapper: '',
+                }}
+              />
+              <Input
+                aria-label="input-due-date"
+                type="date"
+                label="Due date"
+                className="max-w-40"
+                classNames={{
+                  label: 'group-data-[filled=true]:text-secondary',
+                  input:
+                    'group-data-[has-value=true]:text-primary group-data-[has-value=true]:dark:text-white',
+                }}
+              />
+            </div>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onPress={handleUpdateTask}>
+            Update
+          </Button>
+        </ModalFooter>
       </ModalContent>
     </Modal>
   );
