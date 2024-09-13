3e71d9695e1dc1e12cf7f9f6b555a646ce6a9955
du.nguyen <du.nguyen@asnet.com.vn>
2024-09-13 15:10:21 +0700

    fix: update nft page responsive

--- src/ui/nft-marketplace/NFTRecentlyAdd/index.tsx
+++ src/ui/nft-marketplace/NFTRecentlyAdd/index.tsx
@@ -1,23 +1,51 @@
+'use client';
+
 // Models
 import { NFT } from '@/models/NFT';
 
-import { Text } from '@/components';
 import NFTCard from '../NFTCard';
+import { Button, Text } from '@/components';
+
+// Hooks
+import useScroll from '@/hooks/useScroll';
+
+// Icons
+import { ArrowLeft, ArrowRight } from '@/icons';
 
 interface NTFRecentlyAddProps {
   recentlyList: NFT[];
 }
 
 const NTFRecentlyAdd = ({ recentlyList }: NTFRecentlyAddProps) => {
-  // TODO: handle call api get recently list
+  const { ref, handleScrollLeft, handleScrollRight } =
+    useScroll<HTMLDivElement>();
 
   return (
-    <div>
+    <div className="">
       <div className="flex justify-between pb-5">
         <Text className="font-bold text-[24px]">Recently Added</Text>
+        <div className="flex gap-5">
+          <Button
+            isIconOnly
+            onClick={handleScrollLeft}
+            className="bg-white dark:bg-default [&_path]:stroke-blue-450 [&_path]:dark:stroke-white rounded-full"
+          >
+            <ArrowLeft />
+          </Button>
+          <Button
+            isIconOnly
+            onClick={handleScrollRight}
+            className="bg-white dark:bg-default [&_path]:stroke-blue-450 [&_path]:dark:stroke-white rounded-full"
+          >
+            <ArrowRight />
+          </Button>
+        </div>
       </div>
 
-      <div className="flex flex-wrap gap-5">
+      <div
+        className="flex gap-5 overflow-x-auto scroll-smooth hide-scrollbar"
+        ref={ref}
+      >
         {recentlyList.map((item) => (
           <NFTCard
             key={item.id}
