4cb2f71c3685d9f5d5f3740d6ec1a22cbc6c74b6
du.nguyen <du.nguyen@asnet.com.vn>
2024-09-13 15:26:47 +0700

    fix: update nft history

--- src/ui/nft-marketplace/NFTHistory/index.tsx
+++ src/ui/nft-marketplace/NFTHistory/index.tsx
@@ -20,7 +20,7 @@ interface NFTHistoryProps {
 
 const NFTHistory = ({ historyList }: NFTHistoryProps) => (
   <div className="flex flex-col bg-white dark:bg-indigo rounded-md w-full 4xl:max-w-nftCard overflow-auto shadow-md pb-[46px] pt-[19px]">
-    <div className="flex items-center justify-between p-[18px]">
+    <div className="flex items-center justify-between p-3 xs:p-[18px]">
       <Text className="leading-8 font-poppins font-bold text-blue-450">
         History
       </Text>
@@ -30,11 +30,11 @@ const NFTHistory = ({ historyList }: NFTHistoryProps) => (
       </Button>
     </div>
 
-    <div className="flex justify-center flex-col px-[18px]">
+    <div className="flex justify-center flex-col px-3 xs:px-[18px]">
       {historyList.map(({ image, title, author, price, timeAgo }, index) => (
         <div
           key={index}
-          className="flex gap-8 justify-between p-5 rounded-md hover:shadow-md hover:dark:bg-indigo-light"
+          className="flex gap-8 justify-between p-4 xs:p-5 rounded-md hover:shadow-md hover:dark:bg-indigo-light"
         >
           <div className="flex items-center gap-4">
             <Image
@@ -42,7 +42,7 @@ const NFTHistory = ({ historyList }: NFTHistoryProps) => (
               alt={title}
               width={66}
               height={66}
-              className="rounded-xl w-[66px] h-[66px] object-cover hidden xs:block"
+              className="rounded-xl w-10 h-10 xs:w-[66px] xs:h-[66px] object-cover"
             />
             <div className="flex flex-col">
               <Text className="leading-tiny font-bold">Colorful Heaven</Text>
@@ -56,7 +56,7 @@ const NFTHistory = ({ historyList }: NFTHistoryProps) => (
             <Text className="font-bold">{price} $</Text>
           </div>
 
-          <div className="flex items-center">
+          <div className="hidden xs:flex items-center">
             <Text className="text-secondary">
               {formatDateRelativeToNow(timeAgo)}
             </Text>
