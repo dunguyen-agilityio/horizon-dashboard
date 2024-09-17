9de8729bccc27dae39a63a1aceb0e8d3db2f0637
Viet Pham <viet.pham@asnet.com.vn>
2024-09-17 16:47:25 +0700

    (Feat: #86) Define QUERY, POPULATE constants

---
+++ src/constants/query.ts
@@ -0,0 +1,9 @@
+export const QUERY = {
+  PAGINATION_PAGE: (page: string) => `pagination[page]=${page}`,
+  PAGINATION_PAGESIZE: (pageSize: string) => `pagination[pageSize]=${pageSize}`,
+};
+
+export const POPULATE = {
+  user: 'users_permissions_user',
+  nfts: 'nfts',
+};
