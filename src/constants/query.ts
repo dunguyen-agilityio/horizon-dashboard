bef958c826de0b895364761b3e817dbb948d4b7d
Viet Pham <viet.pham@asnet.com.vn>
2024-09-18 16:38:48 +0700

    (Feat: #148) Define PAGE_SIZE constant and remove ContributorTableContainer component

--- src/constants/query.ts
+++ src/constants/query.ts
@@ -2,3 +2,9 @@ export const QUERY = {
   PAGINATION_PAGE: (page: string) => `pagination[page]=${page}`,
   PAGINATION_PAGESIZE: (pageSize: string) => `pagination[pageSize]=${pageSize}`,
 };
+
+export const PAGE_SIZE = {
+  SMALL: '5',
+  MEDIUM: '8',
+  HIGH: '10',
+};
