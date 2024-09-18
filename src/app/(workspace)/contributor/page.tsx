07851b1dd76a7bb90a384b3e883f7423a6838e7a
Viet Pham <viet.pham@asnet.com.vn>
2024-09-18 17:59:11 +0700

    (Feat: #148) Hanlde fetching contributor data inside contributor route

--- src/app/(workspace)/contributor/page.tsx
+++ src/app/(workspace)/contributor/page.tsx
@@ -1,16 +1,60 @@
-import { Suspense } from 'react';
+// Components
+import { ContributorTable } from '@/ui/contributor';
+import ErrorFallback from '@/components/ErrorFallback';
+import DateRangePicker from '@/components/DateRangePicker';
+
+// Constants
+import { PAGE_SIZE } from '@/constants/query';
+
+// Services
+import { getContributor } from '@/services/contributor';
 
 // Types
 import { ISearchParams } from '@/types/query-params';
 
-// Components
-import ContributorTable from '@/ui/contributor/ContributorContainer';
-import { ContributorTableSkeleton } from '@/components/Skeleton/ContributorTableSkeleton';
+// Utils
+import { mapContributorsData } from '@/utils/contributor';
 
-const ContributorPage = ({ searchParams }: { searchParams: ISearchParams }) => (
-  <Suspense fallback={<ContributorTableSkeleton />}>
-    <ContributorTable searchParams={searchParams} />
-  </Suspense>
-);
+const ContributorPage = async ({
+  searchParams,
+}: {
+  searchParams: ISearchParams;
+}) => {
+  const { page = '1', startDate, endDate } = searchParams;
+
+  const { data, error } = await getContributor({
+    page,
+    startDate: startDate || '2024-09-13',
+    endDate: `${endDate}T23:59:59.999Z` || '2024-09-18T23:59:59.999Z',
+    pageSize: PAGE_SIZE.MEDIUM,
+    cacheOptions: 'no-store',
+  });
+
+  if (error)
+    return (
+      <ErrorFallback
+        message={error.message}
+        className="h-[860px] bg-gray dark:bg-indigo-dark"
+      />
+    );
+
+  const { data: dataContributor, meta } = data;
+  const { pageCount = 0 } = meta?.pagination || {};
+
+  const dataFormatted = mapContributorsData(dataContributor);
+
+  return (
+    <>
+      <div className="flex w-full justify-center mt-4 xs:justify-end">
+        <DateRangePicker />
+      </div>
+      <ContributorTable
+        data={dataFormatted}
+        page={parseInt(page, 10)}
+        pageCount={pageCount}
+      />
+    </>
+  );
+};
 
 export default ContributorPage;
