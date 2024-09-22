50c49ad55324bb31d1718b29eb95fbe21e9becb2
Viet Pham <viet.pham@asnet.com.vn>
2024-09-22 16:00:06 +0700

    (Feat: #168) Modified Upload session become input element to upload file

--- src/ui/profile/Upload/index.tsx
+++ src/ui/profile/Upload/index.tsx
@@ -9,8 +9,9 @@ import { BoxIcon, Button, Text } from '@/components';
 
 const UploadProfile = () => (
   <div className="flex flex-col sm:flex-row items-center gap-9 p-7 bg-white rounded-md dark:bg-indigo sm:h-[365px]">
-    <div className="">
-      <div className="flex flex-col gap-6 justify-center items-center bg-grayest dark:bg-indigo-light w-[268px] h-[311px] rounded-md border-dashed border-2 border-amber-450 dark:border-secondary">
+    <div>
+      <label className="flex flex-col gap-6 justify-center items-center bg-grayest dark:bg-indigo-light w-[268px] h-[311px] rounded-md border-dashed border-2 border-amber-450 dark:border-secondary">
+        <input type="file" className="hidden" />
         <BoxIcon
           icon={<UploadFile />}
           customClass="w-12 h-14fill-indigo-light fill-blue-450 dark:fill-white"
@@ -26,7 +27,7 @@ const UploadProfile = () => (
             PNG, JPG and GIF files are allowed
           </Text>
         </div>
-      </div>
+      </label>
     </div>
     <div className="flex flex-col justify-around text-center sm:text-start">
       <div>
