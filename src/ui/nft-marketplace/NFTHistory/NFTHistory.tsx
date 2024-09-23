c98b83eaefb0d1e58f5f7ebcec502e028c713b27
du.nguyen <du.nguyen@asnet.com.vn>
2024-09-23 09:04:32 +0700

    fix: nft history missing nft name from api

--- src/ui/nft-marketplace/NFTHistory/NFTHistory.tsx
+++ src/ui/nft-marketplace/NFTHistory/NFTHistory.tsx
@@ -44,7 +44,7 @@ const NFTHistory = ({ historyList }: NFTHistoryProps) => (
                 className="rounded-xl w-10 h-10 xs:w-[66px] xs:h-[66px] object-cover"
               />
               <div className="flex flex-col">
-                <Text className="leading-tiny font-bold">Colorful Heaven</Text>
+                <Text className="leading-tiny font-bold">{name}</Text>
                 <Text className="leading-small text-small text-secondary">
                   By {author.fullName}
                 </Text>
