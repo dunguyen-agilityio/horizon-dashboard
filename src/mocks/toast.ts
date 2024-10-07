60f9559c0e2e44f84f0283473700f59b0e03c5da
Long Viet Tran <long.tran@asnet.com.vn>
2024-10-07 09:55:36 +0700

    Fix: #257 should increase time for toast

--- src/mocks/toast.ts
+++ src/mocks/toast.ts
@@ -4,54 +4,54 @@ export const ADD_TASK_SUCCESS = {
   title: 'Success',
   message: 'Your task was submitted successfully',
   type: TOAST.SUCCESS,
-  timeOut: 3000,
+  timeOut: 5000,
 };
 
 export const ADD_TASK_ERROR = {
   title: 'Error',
   message: 'There was an error submitting your task',
   type: TOAST.ERROR,
-  timeOut: 3000,
+  timeOut: 5000,
 };
 
 export const UPDATE_TASK_SUCCESS = {
   title: 'Success',
   message: 'The task has been successfully updated.',
   type: TOAST.SUCCESS,
-  timeOut: 3000,
+  timeOut: 5000,
 };
 
 export const UPDATE_TASK_ERROR = {
   title: 'Error',
   message: 'The task has failed to update.',
   type: TOAST.ERROR,
-  timeOut: 3000,
+  timeOut: 5000,
 };
 
 export const REMOVE_TASK_SUCCESS = {
   title: 'Success',
   message: 'The task has been successfully removed.',
   type: TOAST.SUCCESS,
-  timeOut: 3000,
+  timeOut: 5000,
 };
 
 export const REMOVE_TASK_ERROR = {
   title: 'Error',
   message: 'The task has failed to remove.',
   type: TOAST.ERROR,
-  timeOut: 3000,
+  timeOut: 5000,
 };
 
 export const UPDATE_START_DATE_ERROR = {
   title: 'Error',
   message: 'Start date must be less than due date',
   type: TOAST.ERROR,
-  timeOut: 3000,
+  timeOut: 5000,
 };
 
 export const UPDATE_DUE_DATE_ERROR = {
   title: 'Error',
   message: 'Start date must be greater than end date',
   type: TOAST.ERROR,
-  timeOut: 3000,
+  timeOut: 5000,
 };
